73ab644e841750669b260df0b210d6b477aa9969
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-08-04 15:56:43 -0400

    using shutil for cross device support

--- src/organizer/phone.py
+++ src/organizer/phone.py
@@ -1,5 +1,6 @@
 import logging
-from os import listdir, makedirs, rename
+from os import listdir, makedirs
+import shutil
 from os.path import isfile, join, exists, basename
 
 import exiftool
@@ -35,7 +36,7 @@ def organize_phone_photos(source, destiny):
     files = [join(source, f) for f in listdir(source)
              if isfile(join(source, f)) and not f.startswith('.')]
 
-    logger.info('Files in source directory: %s', files)
+    logger.info('%i files in source directory', len(files))
 
     # create folders if they do not exist
     for folder in folders.values():
@@ -49,7 +50,7 @@ def organize_phone_photos(source, destiny):
     # go through every image and place it in the folder they belong
     for file, media_type in tqdm(get_image_type(files)):
         folder = folders[media_type]
-        rename(file, join(destiny, folder, basename(file)))
+        shutil.move(file, join(destiny, folder, basename(file)))
 
 
 def _get_single_image_type(metadata):
